54b33cb8708dc55dec014c84dce916012bc7f208
Anthony Wan <anthonywan00@gmail.com>
2019-04-30 18:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else {
-    return `
-  }
+    return `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`;
   }
 }
