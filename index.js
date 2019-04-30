e8dab2c8d03ed4a35d0904a80588dafe3fb9fc2a
Anthony Wan <anthonywan00@gmail.com>
2019-04-30 18:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ function placeOrder(cardNumber) {
   }
   else {
     cart = [];
-    return `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
